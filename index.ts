79495e955b676fe88b0937c4e4b0d6ad5d7df0e0
lokeshjha <lokesh1jha@gmai.com>
2024-01-28 17:23:07 +0530

    uncommented anf fixed the route

--- index.ts
+++ index.ts
@@ -1,10 +1,14 @@
 import { Hono } from 'hono'
+import bookRouter from "./routes/book"
+
 
 const app = new Hono()
 
 // console.log("Hello via Bun!");
+app.route("/book", bookRouter)
 
 app.get('/', (c) => c.text('Hello Bun!'))
+
 
 
 Bun.serve({
