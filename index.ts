fc948cab67d7d868f261f50e0b749d4ea2b568ff
lokeshjha <lokesh1jha@gmai.com>
2024-01-28 19:47:47 +0530

    streaming simple

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import { Hono } from 'hono'
 import bookRouter from "./routes/book"
+import { streamText } from 'hono/streaming'
 
 
 const app = new Hono()
@@ -7,9 +8,22 @@ const app = new Hono()
 // console.log("Hello via Bun!");
 app.route("/book", bookRouter)
 
+app.get('/stream', (c) => {
+    return streamText(c, async (stream) => {
+        for (let i = 1; i <= 10; i++) {
+            await stream.sleep(1000)
+            await stream.writeln(i.toString());
+        }
+      // Write a text with a new line ('\n').
+      await stream.writeln('Hello')
+      // Wait 1 second.
+      await stream.sleep(1000)
+      // Write a text without a new line.
+      await stream.write(`Hono!`)
+    })
+  })
+
 app.get('/', (c) => c.text('Hello Bun!'))
-
-
 
 Bun.serve({
     fetch: app.fetch,
